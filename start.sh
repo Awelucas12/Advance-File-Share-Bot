e8c2806eeb1eb579a4179c045418fbfd54c1a761
Awelucas12 <168267433+Awelucas12@users.noreply.github.com>
2024-04-27 12:12:18 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Abhishekk6202/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/Abhishekk6202/Advance-File-Share-Bot /zion-film-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+  git clone $UPSTREAM_REPO /zion-film-Bot
 fi
-cd /Advance-File-Share-Bot
+cd /zion-film-Bot
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
